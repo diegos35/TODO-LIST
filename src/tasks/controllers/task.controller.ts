8e7caaa39c71829c0f818c3ccd60cc42ce694652
diego <diego_1996.c.c@gmail.com>
2024-10-18 16:04:01 -0500

    add CRUD operations

--- src/tasks/controllers/task.controller.ts
+++ src/tasks/controllers/task.controller.ts
@@ -7,6 +7,44 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { TasksService } from '../services/tasks.service';
+import { Task } from '../entities/task.entity';
+import { TaskStatus } from '../enums/task-status.enum';
 
-@Controller({})
-export class TaslController {}
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  create(@Body() body: { title: string; description?: string }): Task {
+    return this.tasksService.createTask(body.title, body.description);
+  }
+
+  @Get()
+  findAll(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() body: { title?: string; description?: string; status?: TaskStatus },
+  ): Task {
+    return this.tasksService.updateTask(
+      id,
+      body.title,
+      body.description,
+      body.status,
+    );
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): void {
+    this.tasksService.deleteTask(id);
+  }
+}
